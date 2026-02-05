d2e0d7edd436f3e3b118225efc0466da9b0e91e0
redteam <redteam@local>
2026-02-06 06:30:57 +0800

    fix MainActivity

--- app/src/main/java/com/xxx/app/ui/MainActivity.kt
+++ app/src/main/java/com/xxx/app/ui/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.xxx.app.ui
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.xxx.app.R
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity() { override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState); setContentView(R.layout.activity_main) } }
