2ff38df79cf8e7e113b023a2f91dd3233cefd66e
redteam <redteam@local>
2026-02-06 06:14:48 +0800

    fix: recreate MainActivity in correct package path

---
+++ app/src/main/java/com/xxx/app/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.xxx.app.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.xxx.app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
