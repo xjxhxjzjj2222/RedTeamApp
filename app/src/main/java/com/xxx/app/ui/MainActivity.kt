97ae1b599ed49ff971502eb8638f41c7aa47a174
redteam <redteam@local>
2026-02-06 07:42:53 +0800

    Fix MainActivity compilation (ComponentActivity)

--- app/src/main/java/com/xxx/app/ui/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.xxx.app.ui
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.xxx.app.R
-class MainActivity : AppCompatActivity() { override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState); setContentView(R.layout.activity_main) } }
